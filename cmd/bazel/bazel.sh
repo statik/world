b2d69f15ed98ed0e80f745298c1606f255eb1405
Will Maier <wcmaier@gmail.com>
2019-04-14 10:38:42 -0700

    x

--- cmd/bazel/bazel.sh
+++ cmd/bazel/bazel.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 URL="https://github.com/bazelbuild/bazel/releases/download/0.24.1/bazel-0.24.1-linux-x86_64"
 SHA256SUM="e18e2877e18a447eb5d94f5efbec375366d82af6443c6a83a93c62657a7b1c32"
-CACHE="${CACHE:-/tmp}/whilp_world}"
+CACHE="${CACHE:-/tmp}/whilp_world"
 
 main () {
   tmp="${CACHE}/tmp"
