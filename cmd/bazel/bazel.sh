f733ca77dbeafcdde1bbfb8a6cbaaae729fb4095
Will Maier <wcmaier@gmail.com>
2019-04-14 10:43:40 -0700

    h

--- cmd/bazel/bazel.sh
+++ cmd/bazel/bazel.sh
@@ -4,7 +4,8 @@
 
 URL="https://github.com/bazelbuild/bazel/releases/download/0.24.1/bazel-0.24.1-linux-x86_64"
 SHA256SUM="e18e2877e18a447eb5d94f5efbec375366d82af6443c6a83a93c62657a7b1c32"
-CACHE="${CACHE:-/tmp}/whilp_world"
+#CACHE="${CACHE:-/tmp}/whilp_world"
+CACHE="/tmp/whilp_world"
 
 main () {
   tmp="${CACHE}/tmp"
@@ -16,11 +17,11 @@ main () {
   mkdir -p "$bin"
   mkdir -p "$tmp"
   
-  fetch "$URL" "${tmp}/bazel
-  checksum "$SHA256SUM" "${tmp}/bazel
+  fetch "$URL" "${tmp}/bazel"
+  checksum "$SHA256SUM" "${tmp}/bazel"
   
-  chmod a+x "${tmp}/bazel
-  mv "${tmp}/bazel "${bin}/bazel"
+  chmod a+x "${tmp}/bazel"
+  mv "${tmp}/bazel" "${bin}/bazel"
   
   exec bazel "$@"
 }
