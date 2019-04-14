6f8c7d84ce1230c20763a988a18296608a18db34
Will Maier <wcmaier@gmail.com>
2019-04-14 10:41:44 -0700

    g

--- cmd/bazel/bazel.sh
+++ cmd/bazel/bazel.sh
@@ -1,13 +1,12 @@
 #/usr/bin/env bash
 
-set -euo pipefail
+#set -euo pipefail
 
 URL="https://github.com/bazelbuild/bazel/releases/download/0.24.1/bazel-0.24.1-linux-x86_64"
 SHA256SUM="e18e2877e18a447eb5d94f5efbec375366d82af6443c6a83a93c62657a7b1c32"
 CACHE="${CACHE:-/tmp}/whilp_world"
 
 main () {
-set -x
   tmp="${CACHE}/tmp"
   bin="${CACHE}/bin"
   export PATH="${PATH}:${bin}"
@@ -36,9 +35,7 @@ fetch () {
 checksum () {
   sha256sum="$1"
   src="$2"
-  (
   printf "${sha256sum}\t${src}\n" | sha256sum -c
-  ) >/dev/null 2>&1
 }
 
 main "$@"
