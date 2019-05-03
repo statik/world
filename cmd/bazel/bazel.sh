46dccdfa91fd86773bf3294470701f17dc807371
Will Maier <wcmaier@gmail.com>
2019-05-03 05:30:40 +0000

    lol

--- cmd/bazel/bazel.sh
+++ cmd/bazel/bazel.sh
@@ -1,29 +1,30 @@
 #/usr/bin/env bash
 
 #set -euo pipefail
+set -x
 
 URL="https://github.com/bazelbuild/bazel/releases/download/0.24.1/bazel-0.24.1-linux-x86_64"
 SHA256SUM="e18e2877e18a447eb5d94f5efbec375366d82af6443c6a83a93c62657a7b1c32"
-#CACHE="${CACHE:-/tmp}/whilp_world"
-CACHE="/tmp/whilp_world"
+BIN="$PWD/bin"
+DST="$BIN/bazel"
 
 main () {
-  tmp="${CACHE}/tmp"
-  bin="${CACHE}/bin"
-  export PATH="${PATH}:${bin}"
-  
-  command -v bazel && exec bazel "$@"
-  
-  mkdir -p "$bin"
-  mkdir -p "$tmp"
-  
-  fetch "$URL" "${tmp}/bazel"
-  checksum "$SHA256SUM" "${tmp}/bazel"
-  
-  chmod a+x "${tmp}/bazel"
-  mv "${tmp}/bazel" "${bin}/bazel"
+  export PATH="${PATH}:${BIN}"
+
+  command -v bazel >/dev/null && exec bazel "$@"
+
+  mkdir -p "$BIN"
+  trap clean EXIT
+
+  fetch "$URL" "${BIN}/bazel"
+  checksum "$SHA256SUM" "${BIN}/bazel"
+  chmod a+x "${BIN}/bazel"
   
   exec bazel "$@"
+}
+
+clean () {
+  rm -f "$DST"
 }
 
 fetch () {
