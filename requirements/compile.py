50fd7b2e8fae68c4aa60d87870f8fab6fbf4663a
Will Maier <189851+whilp@users.noreply.github.com>
2020-07-06 11:19:01 -0700

    enable python (#37)
    * configure venv
    
    * clobber XDG_CACHE_HOME
    
    * test requirements
    
    * enable python
    
    * defer imports
    
    * fix: set LANG and LC_ALL
    
    * pin setuptools
    
    * set temporary cache
    
    * skip test

--- requirements/compile.py
+++ requirements/compile.py
@@ -2,14 +2,26 @@
 
 import os
 import sys
-
-import piptools.scripts.compile as pip_compile
+import tempfile
 
 
 def main(args, env):
-    output_file = os.path.join(env.get("BUILD_WORKSPACE_DIRECTORY", "."), "requirements.txt")
-    extra = [f"--output-file={output_file}"]
-    pip_compile.cli(args[1:] + extra)
+    output_file = os.path.abspath(os.path.join(env.get("BUILD_WORKSPACE_DIRECTORY", "."), "requirements.txt"))
+    os.environ.update(env, CUSTOM_COMPILE_COMMAND="bazel run requirements:compile")
+    extra = [f"--output-file={output_file}", "--generate-hashes", "--allow-unsafe", "--no-header"]
+
+    with tempfile.TemporaryDirectory() as tmp:
+        os.environ["XDG_CACHE_HOME"] = os.path.join(tmp, "cache")
+        return run(args[1:] + extra, env)
+
+
+def run(args, env):
+    import piptools.scripts.compile as pip_compile
+
+    try:
+        return pip_compile.cli(args)
+    except SystemExit:
+        pass
 
 
 if __name__ == "__main__":
---
+++ requirements/test.py
@@ -0,0 +1,42 @@
+import os
+import unittest
+
+
+class TestCase(unittest.TestCase):
+    maxDiff = None
+
+    def test_nochange(self):
+        self.skipTest("hashes differ slightly in CI :/")
+
+        requirements_in = "requirements.in"
+        requirements_txt = "requirements.txt"
+
+        build_workspace_directory = "build_workspace_directory"
+        os.makedirs(build_workspace_directory, exist_ok=True)
+
+        env = dict(BUILD_WORKSPACE_DIRECTORY=build_workspace_directory)
+        args = [requirements_in]
+
+        run(args, env)
+
+        want = read(requirements_txt)
+        got = read(os.path.join(build_workspace_directory, requirements_txt))
+
+        self.assertEqual(want, got)
+
+
+def run(args, env):
+    import compile
+
+    try:
+        return compile.main(["test-prog"] + args, env)
+    except SystemExit:
+        pass
+
+
+def read(path):
+    with open(path) as f:
+        return f.read()
+
+
+unittest.main()
