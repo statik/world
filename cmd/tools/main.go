962e8b4786200a2765e76e67a7251d09dd0911c4
Will Maier <189851+whilp@users.noreply.github.com>
2020-09-13 11:11:03 -0700

    feat: add go tools (#352)
    * feat: add go tools
    
    * lint less
    
    * fix: zap file to exclude from analysis
    
    * fix: restore cmd/tools after lint

---
+++ cmd/tools/main.go
@@ -0,0 +1,14 @@
+// +build tools
+//nolint
+package main
+
+// https://github.com/golang/go/issues/25922#issuecomment-414677877
+import (
+	_ "github.com/mdempsky/gocode"
+	_ "github.com/ramya-rao-a/go-outline"
+	_ "github.com/rogpeppe/godef"
+	_ "github.com/uudashr/gopkgs/v2/cmd/gopkgs"
+	_ "golang.org/x/lint/golint"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "golang.org/x/tools/cmd/guru"
+)
